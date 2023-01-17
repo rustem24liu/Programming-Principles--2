b9970ff67bf9dfbd7e9cf5017e594af95f22e062
liu rus <temirgalirustem05@gmail.com>
2023-01-17 17:21:28 +0600

    new py

---
+++ lecture1.py
@@ -0,0 +1,19 @@
+'''salary = 8000
+def printSalary():
+    salary = 12000
+    print("Salary", salary)
+printSalary()
+print("Salary, salary)
+'for x in range(0.5, 5.5, 0.5):
+    print(x)
+def calculate(num1, num2):
+    res = num1 * num2
+    print(res)
+calculate(5, 6)'''
+# str = "James Bond"
+# print(str[2::-1])
+strings  = ["string", "time", "velocity"]
+strings.append(1, "SCOTT")
+print(strings)
+# git comit -m "name"
+# git config
