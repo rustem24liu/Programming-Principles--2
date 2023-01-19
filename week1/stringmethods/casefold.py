8acf2843dc5e8af2a2879ec34a667e397ec2087d
Rustem <120200923+rustem24liu@users.noreply.github.com>
2023-01-19 22:34:09 +0600

    Add files via upload

---
+++ week1/stringmethods/casefold.py
@@ -0,0 +1,3 @@
+a = "HELLO, WORLD"
+print(a.casefold())
+print(a.lower())
---
+++ week1/stringmethods/count.py
@@ -0,0 +1,8 @@
+a = "Hello, World!"
+print(a.count("H"))
+print(a.count("h"))
+print(a.count("e"))
+print(a.count("l"))
+print(a.count("o"))
+print(a.count("W"))
+print(a.count("d"))
