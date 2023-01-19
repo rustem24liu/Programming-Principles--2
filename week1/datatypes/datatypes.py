6cbb90c2e10954a6057e4bb9087f57004607548c
Rustem <120200923+rustem24liu@users.noreply.github.com>
2023-01-19 16:27:36 +0600

    Add files via upload

---
+++ week1/datatypes/datatypes.py
@@ -0,0 +1,26 @@
+x , y, z, w = [4, 4.56, 5e+10, 5j ]
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(w))
+
+
+a, b, c = "street", True, False
+print(type(a))
+print(type(b))
+print(type(c))
+
+d = range(6, 1)
+print(d)
+print(type(d))
+
+# LIST
+A = [4.5, "street", True]
+print(type(A))
+
+B = (4.5, "street", 56j)
+print(type(B))
+
+# SET
+C = {4.5, "street", 56j}
+print(type(C))
---
+++ week1/datatypes/datatypes2.py
@@ -0,0 +1,6 @@
+print(5j)
+print(type(5j))
+x = 6 + 5j
+print(type(x))
+x1 =complex(5,6)
+print(x1)
