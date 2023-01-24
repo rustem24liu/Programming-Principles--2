1323d6f36ad6e02936fc0201fdab3d566c400cbd
liu rus <temirgalirustem05@gmail.com>
2023-01-24 16:09:21 +0600

    new py

--- list/changelist/changelist.py
+++ list/changelist/changelist.py
@@ -1,3 +1,5 @@
 thislist = ["apple", "banana", "cherry"]
 thislist[1] = "blackcurrant"
+thislist[0] = "rustem"
+thislist[2] = "ayan"
 print(thislist)
---
+++ list/changelist/changelist5.py
@@ -0,0 +1,5 @@
+# insert()
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(0, "watermelon")
+thislist.insert(3, "wine")
+print(thislist)
